cd23a3174117a0ecdbe12874fa04d8d9d5e9a1b3
João Renato Pinto <jrcp210@gmail.com>
2021-02-27 13:53:10 +0000

    Added a frog, death animation and dmg to bullets

---
+++ Contra-Sigarra/Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+
+    public GameObject deathEffect;
+
+    public void TakeDamage(int damage) 
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            StartCoroutine(Die());
+        }
+    }
+    
+    IEnumerator Die()
+    {
+        GameObject deathAnim = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        yield return 0;
+        Destroy(deathAnim);
+    }
+}
--- Contra-Sigarra/Assets/Scripts/Weapon Scripts/Bullet.cs
+++ Contra-Sigarra/Assets/Scripts/Weapon Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
 
     public float speed = 20f;
     public Rigidbody2D rb;
+    public int damage = 40;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,11 @@ public class Bullet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D hitInfo) 
     {
-        Debug.Log(hitInfo.name);
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if(enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
         Destroy(gameObject);
     }
 }
