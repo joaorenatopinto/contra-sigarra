8f64ecd38dacfa81f9aab730c275136649a078be
david <david-elam@hotmail.com>
2021-03-05 21:43:19 +0000

    added enemy movement and score

---
+++ Contra-Sigarra/Assets/Scripts/Enemy/enemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+
+    public float enemySpeed = 0.1f;
+    Vector2 rayLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayLine = Vector2.right + Vector2.down;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+          
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayLine, 3);
+        if (hit.collider == null)
+        {
+            
+            rayLine.x = -rayLine.x;
+            enemySpeed = -enemySpeed;
+        }
+
+        transform.position = new Vector2(transform.position.x + this.enemySpeed * Time.fixedDeltaTime, transform.position.y);
+
+
+    }
+}
+    
---
+++ Contra-Sigarra/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    public GameObject enemies;
+    private float nrEnemies;
+    private void Start()
+    {
+        nrEnemies = enemies.transform.childCount;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = (((nrEnemies - enemies.transform.childCount)/nrEnemies)*20).ToString();
+    }
+}
