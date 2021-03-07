bb276071693a5bdd039caeda07d3060c93bc3927
david <david-elam@hotmail.com>
2021-03-07 12:07:43 +0000

    small adjustments

--- Contra-Sigarra/Assets/Scripts/Movement/PlayerMovement.cs
+++ Contra-Sigarra/Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public GameObject Platforms;
+
+
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
 
 
-    // Update is called once per frame
+
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
@@ -20,10 +21,7 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetButtonDown("Jump"))
         {
             jump = true;
-
         }
-
-
     }
 
 
